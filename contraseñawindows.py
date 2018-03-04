a3988042a77cbe0659ede2eb35bfc5868a6596a5
george39 <george35@utp.edu.co>
2018-03-03 20:58:43 -0500

    pasos para quitar o combiar la contraseña de windows

---
+++ contraseñawindows.py
@@ -0,0 +1,19 @@
+               PASOS PARA QUITAR O CAMBIAR LA CONTRASEÑA DE WINDOWS SI LA OLVIDAMOS
+
+1. cuando este iniciando windows apagar el computador a la fuerza 
+2. luego enceder y elegir la opcion iniciar reparacion de inicio 
+3. cuando salga la ventana reparacion de inicio dar clic en cancelar
+4. en la pantalla que sale dar clicl en ver detalles del problema y clic en el ultimo link 
+5. en la ventana siguiente clic en archivo- abrir
+6. seleccione equipo y buscar disco local 
+7. abrir carpeta windows 
+8. en tipo seleccionar todos los archivos 
+9. abrir carpeta system 32
+10. buscar archivo sethc y renombrarlo 
+11. buscar cmd, copiarlo y pegarlo en lo ultimo y cambiar el nombre de cmd por sethc
+12. cerrar las ultimas dos ventanas y en la que queda click en no enviar y despues en finalizar
+13. cuando el equipo reinicie y llegue al usuario precionar 5 veces la tecla shif
+14. cuando aparesca cmd poner el comando net user y enter
+15. cuando apareScan los usuarios ingresar el comando net user nombredelusuario * y enter
+16. lo siguiente es ingresar la nueva contraseña o si no la quiere presione enter 
+17. cerrar cmd e ingresar y listo 
